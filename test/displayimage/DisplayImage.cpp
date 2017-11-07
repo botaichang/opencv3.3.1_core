82f5c6e7a971aaed98572925bd20d3e4ba54fdf2
junli <junli.westwell-lab.com>
2017-11-07 14:28:14 +0800

    test program

---
+++ test/displayimage/DisplayImage.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+  if( argc !=2 )
+  {
+    printf("Usage: DisplayeImage.out <Image_path>\n"); 
+    return -1;
+  }
+  Mat image; 
+  image = cv::imread( argv[1], 1);
+
+  if (!image.data)
+  {
+    printf("No image data\n");
+    return -1;
+  }
+  namedWindow("Display Image",WINDOW_NORMAL);
+  imshow("Display Image",image);
+  waitKey(0);
+  return 0;
+  
+}
