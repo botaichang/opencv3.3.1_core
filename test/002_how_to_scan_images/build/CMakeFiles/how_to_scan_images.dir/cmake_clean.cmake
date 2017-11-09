c012621fcb5b099c761b0a942c8f8a834b97ec6c
junli <junli.westwell-lab.com>
2017-11-09 18:50:49 +0800

    scan_image

---
+++ test/002_how_to_scan_images/build/CMakeFiles/how_to_scan_images.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/how_to_scan_images.dir/how_to_scan_images.o"
+  "how_to_scan_images.pdb"
+  "how_to_scan_images"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/how_to_scan_images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
