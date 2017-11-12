025511ed47df040dbbdd75d2ab8f572fe71dbcef
melchior <melchior_note@163.com>
2017-11-12 19:03:38 +0800

    mask operation

---
+++ test/003_mask_operation/build/CMakeFiles/mask_operation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mask_operation.dir/mat_mask_operations.o"
+  "mask_operation.pdb"
+  "mask_operation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mask_operation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
